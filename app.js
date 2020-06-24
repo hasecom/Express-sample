b8179a8174d6b1b0045b43539fc3d66e84df9a54
hasecom <hasetennisth@gmail.com>
2020-06-24 12:00:44 +0900

    update

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var app = express();
+var http = require('http').Server(app);
+const io =  require('socket.io')(http);
+const PORT = process.env.PORT || 8080;
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection',(socket)=>{
+    socket.on('message',(msg)=>{ 
+        console.log('message:' + msg);
+        io.emit('message',msg);
+    });
+});
+
+http.listen(PORT,()=>{
+    console.log('server listening... Port:' + PORT);
+});
